fdc5c9b4c700daaafb4ecf352109738c75911bea
HuskyyTrzaski <kubajochymek@gmail.com>
2021-02-20 21:25:05 +0100

    first gets

--- serverside.py
+++ serverside.py
@@ -2,48 +2,75 @@ import self as self
 from bs4 import BeautifulSoup
 import requests
 import lxml
+import time
 
 if __name__ == '__main__':
 
     #moze jakis dekorator?
     def returnlink(x):
         return f'https://nofluffjobs.com/pl/jobs/frontend{x}'
-    class JobOffers:
-        def __init__(self,link,requirements=set(),salary=None,level=set()):
-               self.link = link
-               self.requirements = requirements
-               self.salary = salary
-               self.level = level
+
+    data={'link':'None','requirements':set(),'salary':(0,0),'city':'None','level':'None','typeOfemploymennt':'none','typeofJob':'None','companySize':0}
     typesOfJobs=["frontend","backend","ux","artificial-intelligence","fullstack",
-                    "mobile","gaming","big-data","embedded","it-administrator","project-manager","ux","backoffice"]
+                    "mobile","gaming","big-data","embedded","it-administrator",
+                 "project-manager","ux","backoffice"]
 
-    allinks=[]
-
-
-    def zwrocLinkiZOfertamiZeStrony(src):
+    def returnOffersfrompage(src):
         source = requests.get(src).text
         soup = BeautifulSoup(source, 'lxml')
         for elem in soup.find_all('a'):
             link = elem.attrs['href']
             if "/pl/job" in link and link.count("-") > 3:
-                yield "nofluffjobs.com"+link
-    for typeOfJob in typesOfJobs:
-        for i in zwrocLinkiZOfertamiZeStrony(f'https://nofluffjobs.com/pl/jobs/{typeOfJob}'):
-            allinks.append(i)
-        for SiteNumber in range(1):
-            for i in zwrocLinkiZOfertamiZeStrony(f'https://nofluffjobs.com/pl/jobs/{typeOfJob}?page={str(SiteNumber)}'):
-                allinks.append(i)
+                yield "www.nofluffjobs.com"+link
+    def returnOffersFromEveryPage(allLinks=set()):
+        for typeOfJob in typesOfJobs:
+            for i in returnOffersfrompage(f'https://nofluffjobs.com/pl/jobs/{typeOfJob}'):
+                allLinks.add((i,typeOfJob))
+                #wait
 
-    for i in allinks:
-        if allinks.count(i)>1:
-            print(i)
+            for SiteNumber in range(2, 3):
+                for i in returnOffersfrompage(f'https://nofluffjobs.com/pl/jobs/{typeOfJob}?page={str(SiteNumber)}'):
+                    allLinks.add((i,typeOfJob))
 
-    print(len(allinks))
+        return allLinks
+    #fortests
+    import exampledata
+    def getLink(elem):
+        return elem[0]
+    def getRequirements(link):
+        returnList=set()
+        source = requests.get(link).text
+        soup = BeautifulSoup(source, 'lxml')
+        for i in soup.findAll("button", {"class": "btn btn-outline-success btn-sm no-cursor text-truncate"}):
+            returnList.add(i.text)
+        return returnList
 
+    def getSalary(link):
+        returnList = []
+        source = requests.get(link).text
+        soup = BeautifulSoup(source, 'lxml')
+        for i in soup.findAll('h4',class_='mb-0'):
+            returnList.append(i.text)
+        return (returnList[1].split("-")[0].strip(),returnList[1].split("-")[1].replace('PLN','').strip())
+    def getCity(link):
+        returnList =[]
+        source = requests.get(link).text
+        soup = BeautifulSoup(source, 'lxml')
+        try:
+            return soup.find("li", class_='text-break').text.replace('(Po pandemii)', '').strip()
+        except(AttributeError):
+            return "REMOTE"
+    print(getCity('https://nofluffjobs.com/pl/job/java-backend-developer-praca-zdalna-finture-remote-w3dcdr7u'))
 
+    def getLevel(link):
+        pass
+    def getTypeOfJob(link):
+        pass
+    def getComapanySize(link):
+        pass
+    def getTypeOfEmploymennt(link):
+        pass
 
-            #generator
-    #wezmy wszystkie oferty na frontend
 
 
 
